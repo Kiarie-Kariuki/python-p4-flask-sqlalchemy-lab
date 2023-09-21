714744143eea9078e9e6b040e9445ec9b5805987
Collins <collinskiarie13@outlook.com>
2023-09-21 13:46:04 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -19,15 +19,28 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        return f'Animal ID: {animal.id}<br>Name: {animal.name}<br>Species: {animal.species}<br>Zookeeper ID: {animal.zookeeper_id}<br>Enclosure: {animal.enclosure_id}'
+    else:
+        return 'Animal not found'
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        return f'Zookeeper ID: {zookeeper.id}<br>Name: {zookeeper.name}<br>Birthday: {zookeeper.birthday}'
+    else:
+        return 'Zookeeper not found'
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        return f'Enclosure ID: {enclosure.id}<br>Environment: {enclosure.environment}<br>Open to Visitors: {enclosure.open_to_visitors}'
+    else:
+        return 'Enclosure not found'
 
 
 if __name__ == '__main__':
